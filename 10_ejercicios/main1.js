cec73d05695812c3dc05077a96812bb384a88ec1
RonyTCdev <rtcardenas26@gmail.com>
2022-07-09 20:35:03 -0500

    1

---
+++ 10_ejercicios/main1.js
@@ -0,0 +1,12 @@
+function muestraOpcion() {
+
+    var opciones = document.getElementsByName("eleccion");
+    console.log(opciones);
+    for (let i = 0; i < opciones.length; i++) {
+  
+      if (opciones[i].checked) {
+        alert(opciones[i].value);
+      }
+  
+    }
+  }
