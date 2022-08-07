9fe01c265b3b5ce29ad3852da6701397e01f9449
RonyTCdev <rtcardenas26@gmail.com>
2022-08-07 10:48:37 -0500

    probando el setimeout

---
+++ 10_ejercicios/3.demo.js
@@ -0,0 +1,10 @@
+console.log('Javascript es tremendo');
+function saludar(nombre) {
+    console.log(`Hola ${nombre}`)
+}
+
+saludar(`Rony`);
+
+setTimeout(() => {
+    console.log(`Este texto se mostrara despues de 5 segundos`); }, 5000);
+
