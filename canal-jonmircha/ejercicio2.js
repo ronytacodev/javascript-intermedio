f4e13e374066684dbf8123f18fc43219a2a98f00
RonyTCdev <rtcardenas26@gmail.com>
2022-08-15 22:27:38 -0500

    codigo de ejercicio 2

--- canal-jonmircha/ejercicio2.js
+++ canal-jonmircha/ejercicio2.js
@@ -1,3 +1,4 @@
+
 // 2) Programa una función que te devuelva el texto recortado según el número de caracteres indicados, pe. miFuncion("Hola Mundo", 4) devolverá "Hola".
 // 14/08/22
 // Curso JavaScript: 35. Ejercicios de Lógica de Programación ( 2 / 10 ) - #jonmircha del min 12 al 20 min
@@ -9,7 +10,26 @@ const recortarTexto = (cadena = "", longitud = undefined) =>
         ?console.warn ("No ingresaste la longitud a cortar una cadena de texto")
         :console.info (cadena.slice(0, longitud))
 
-recortarTexto("Hola Mundo", 4);
-recortarTexto();
-recortarTexto("Hola Hola");
-recortarTexto("", 5);
+// recortarTexto("Hola Mundo", 4);
+// recortarTexto();
+// recortarTexto("Hola Hola");
+// recortarTexto("", 5);
+
+
+//Curso JavaScript: 35. Ejercicios de Lógica de Programación ( 2 / 10 ) - #jonmircha
+//apartir del min 20 fecha 15/08/22
+
+// 3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal']
+
+const cadenaAArreglo = (cadena="", separador=undefined)=>
+(!cadena)
+?console.warn("No ingresaste una cadena de texto")
+:(separador===undefined)
+?console.warn("No ingresaste el caracter separador")
+:console.info(cadena.split(separador));
+
+cadenaAArreglo("Lorem ipsum dolor sit amet consectetur adipisicing elit. A maiores minus libero in omnis perspiciatis facere, officia architecto, inventore explicabo velit tempora impedit, distinctio aspernatur mollitia quibusdam sit dicta. Praesentium", " ");
+cadenaAArreglo();
+cadenaAArreglo("Hola mundo");
+cadenaAArreglo(""," ");
+
