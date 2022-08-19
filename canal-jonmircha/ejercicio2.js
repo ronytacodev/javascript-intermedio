263cb1af3225d66afe9f177fce2c5e47c98c7f22
RonyTCdev <rtcardenas26@gmail.com>
2022-08-18 23:51:50 -0500

    enviando ejercicios 9,10 y 11

--- canal-jonmircha/ejercicio2.js
+++ canal-jonmircha/ejercicio2.js
@@ -125,6 +125,73 @@ const eliminarCaracteres = (texto = "", patron = "")=>
 ?console.warn("No ingresaste un patrón de caracteres")
 :console.info(texto.replace(new RegExp(patron, "ig"), ""));
 
-eliminarCaracteres();
-eliminarCaracteres("xyz1, xyz2, xyz3, xyz4 y xyz5");
-eliminarCaracteres("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz");
+// eliminarCaracteres();
+// eliminarCaracteres("xyz1, xyz2, xyz3, xyz4 y xyz5");
+// eliminarCaracteres("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz");
+
+
+// 9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
+//Curso JavaScript: 37. Ejercicios de Lógica de Programación ( 4 / 10 ) - #jonmircha 18/08/22 del min 1 al min 4
+
+const aleatorio = () => console.info(Math.round((Math.random()*100)+500));
+
+// aleatorio();
+
+
+
+// 10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
+//Curso JavaScript: 37. Ejercicios de Lógica de Programación ( 4 / 10 ) - #jonmircha 18/08/22 del min 5 al min 14
+
+const capicua = (numero = 0) => {
+    if (!numero) return console.warn("No ingresaste un número");
+
+    if(typeof numero !=="number") return console.error(`El valor "${numero}" ingresado, NO es un número`);
+
+    numero = numero.toString();
+    let alReves = numero.split("").reverse().join("");
+
+    return(numero === alReves)
+    ?console.info(`Si es capícua, Número original ${numero}, Número al revés ${alReves}`)
+    :console.info(`No es capicua, Número original ${numero}, Número al revés ${alReves}`)
+
+}
+
+// capicua();
+// capicua("19");
+// capicua({});
+// capicua(2000);
+// capicua(2002);
+// capicua(18.99);
+// capicua(212.212);
+
+
+
+
+// 11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
+//Curso JavaScript: 37. Ejercicios de Lógica de Programación ( 4 / 10 ) - #jonmircha 18/08/22 del min 15 al min 14
+
+const factorial = (numero = undefined) => {
+    if(numero === undefined) return console.warn("No ingresaste un número");
+
+    if(typeof numero !=="number") return console.error(`El valor "${numero}" ingresado, NO es un número`);
+
+    if(numero === 0) return console.error("El número no puede ser 0");
+
+    if(Math.sign(numero) === -1) return console.error("El número no puede ser negativo");
+
+    let factorial = 1;
+
+    for (let i = numero; i > 1; i--) {
+        factorial = factorial*i; 
+    }
+
+    return console.info(`El factorial de ${numero} es ${factorial}`);
+}
+
+factorial();
+factorial("5");
+factorial([1,2,3]);
+factorial(0);
+factorial(-5);
+factorial(5);
+factorial(8);
