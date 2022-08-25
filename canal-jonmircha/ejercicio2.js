5ffd4b6de2847335ceb91ca2bd8851894a9bb016
RonyTCdev <rtcardenas26@gmail.com>
2022-08-24 23:30:43 -0500

    ejercicios js

--- canal-jonmircha/ejercicio2.js
+++ canal-jonmircha/ejercicio2.js
@@ -198,10 +198,60 @@ const factorial = (numero = undefined) => {
 
 
 // 12) Programa una función que determine si un número es primo (aquel que solo es divisible por sí mismo y 1) o no, pe. miFuncion(7) devolverá true.
+//Curso JavaScript: 38. Ejercicios de Lógica de Programación ( 5 / 10 )  23/08/22  
 
+const numeroPrimo = (numero = undefined) => {
+    if(numero === undefined) return console.warn("No ingresaste un número");
 
+    if(typeof numero !=="number") return console.error(`El valor "${numero}" ingresado, NO es un número`);
 
+    if(numero === 0) return console.error("El número no puede ser 0");
+
+    if(Math.sign(numero) === -1) return console.error("El número no puede ser negativo");
+
+    let divisible = false;
+
+    for(let i=2; i<numero; i++){
+        if((numero%i)===0){
+            divisible=true;
+            break;
+        }
+    }
+
+    return (divisible)
+    ?console.log(`El número ${numero}, NO es primo`)
+    :console.log(`El número ${numero}, SÍ es primo`);
+
+}
+
+// numeroPrimo();
+// numeroPrimo("320");
+// numeroPrimo(true);
+// numeroPrimo(0);
+// numeroPrimo(1);
+// numeroPrimo(-13);
+// numeroPrimo(13);
+// numeroPrimo(200);
 
 
 // 13) Programa una función que determine si un número es par o impar, pe. miFuncion(29) devolverá Impar.
+//Curso JavaScript: 38. Ejercicios de Lógica de Programación ( 5 / 10 )  23/08/22  min 13
+
+const numeroParImpar = (numero = undefined) => {
+    if (numero === undefined) return console.warn("No ingresaste un número");
+
+    if (typeof numero !== "number") return console.error(`El valor "${numero}" ingresado, NO es un número`);
+
+    return ((numero % 2) === 0)
+    ?console.info(`El número ${numero} es Par`)
+    :console.info(`El número ${numero} es Impar`)
+}
+
+// numeroParImpar();
+// numeroParImpar("23");
+// numeroParImpar(-398);
+// numeroParImpar(19);
+
+
+
 // 14) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, pe. miFuncion(0,"C") devolverá 32°F.
