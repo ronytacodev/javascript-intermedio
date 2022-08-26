8d670d865bcd2e4a2c7f7612cc87ac128b5f5696
RonyTCdev <rtcardenas26@gmail.com>
2022-08-25 22:45:01 -0500

    ejercicio 14 js

--- canal-jonmircha/ejercicio2.js
+++ canal-jonmircha/ejercicio2.js
@@ -255,4 +255,36 @@ const numeroParImpar = (numero = undefined) => {
 
 
 // 14) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, pe. miFuncion(0,"C") devolverá 32°F.
-//falta realizar ejercicio
+//Curso JavaScript: 38. Ejercicios de Lógica de Programación ( 5 / 10 )  25/08/22 
+
+const convertirGrados = (grados = undefined, unidad = undefined) => {
+    if (grados === undefined) return console.warn ("No ingresaste grados a convertir");
+
+    if (typeof grados !== "number") return console.error(`El valor "${grados}" ingresado, NO es un númweo`);
+
+    if (unidad === undefined) return console.warn ("No ingresaste el tipo de grado a convertir");
+
+    if(typeof unidad !== "string") return console.error(`El valor "${unidad}" ingresado, NO es una cadena de texto`);
+
+    if(unidad.length !== 1 || !/(C|F)/.test(unidad)) return console.warn("Valor de unidad no reconocido");
+
+    if(unidad === "C"){
+        return console.info(`${grados}°C = ${Math.round((grados * (9/5)) + 32)}°F`);
+    }else if (unidad === "F") {
+        return console.info(`${grados}°F = ${Math.round((grados - 32) * (5/9))}°C`);
+    }else {
+        return console.error("El tipo de grados a convertir NO es válido");
+    }
+
+}
+
+convertirGrados();
+convertirGrados("2");
+convertirGrados(2);
+convertirGrados(2, true);
+convertirGrados(2, "Hola");
+convertirGrados(2, "E");
+convertirGrados(0, "C");
+convertirGrados(100, "C");
+convertirGrados(32, "F");
+convertirGrados(100, "F");
