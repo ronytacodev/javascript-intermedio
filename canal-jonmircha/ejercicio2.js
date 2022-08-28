f15520e5bc26f4ae26a748cc071345aa2f4125cd
RonyTCdev <rtcardenas26@gmail.com>
2022-08-27 23:14:34 -0500

    developing exercise 16

--- canal-jonmircha/ejercicio2.js
+++ canal-jonmircha/ejercicio2.js
@@ -311,15 +311,45 @@ const convertirBinarioDecimal = (numero = undefined, base = undefined) => {
     }
 }
 
-convertirBinarioDecimal();
-convertirBinarioDecimal("2");
-convertirBinarioDecimal(100);
-convertirBinarioDecimal(100, "2");
-convertirBinarioDecimal(100, 2);
-convertirBinarioDecimal(4, 10);
-convertirBinarioDecimal(114, 10);
+// convertirBinarioDecimal();
+// convertirBinarioDecimal("2");
+// convertirBinarioDecimal(100);
+// convertirBinarioDecimal(100, "2");
+// convertirBinarioDecimal(100, 2);
+// convertirBinarioDecimal(4, 10);
+// convertirBinarioDecimal(114, 10);
 
 
 
 // 16) Programa una función que devuelva el monto final después de aplicar un descuento a una cantidad dada, pe. miFuncion(1000, 20) devolverá 800.
+//Curso JavaScript: 39. Ejercicios de Lógica de Programación ( 6 / 10 ) 27/08/22 hasta el min 20 
+
+const aplicarDescuento = (monto = undefined, descuento = 0) => {
+    if (monto === undefined) return console.warn("No ingresaste el monto");
+
+    if (typeof monto !== "number") return console.error(`El valor "${monto}" ingresado, NO es un número`);
+
+    if (monto === 0) return console.error("El monto no puede ser 0");
+
+    if (Math.sign(monto) === -1) return console.error("El monto no puede ser negativo");
+
+    if (typeof descuento !== "number") return console.error(`El valor "${descuento}" ingresado, NO es un número`);
+
+    if (Math.sign(descuento) === -1) return console.error("El descuento no puede ser negativo");
+
+    return console.info(`$${monto} - ${descuento}% = $${monto - ((monto*descuento) / 100)}`);
+}
+
+aplicarDescuento();
+aplicarDescuento("200");
+aplicarDescuento(0);
+aplicarDescuento(-1000);
+aplicarDescuento(1000, "20");
+aplicarDescuento(1000,-20);
+aplicarDescuento(1000);
+aplicarDescuento(1000,25);
+
+
+
+
 // 17) Programa una función que dada una fecha válida determine cuantos años han pasado hasta el día de hoy, pe. miFuncion(new Date(1984,4,23)) devolverá 35 años (en 2020).
