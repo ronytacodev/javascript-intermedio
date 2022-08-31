03c2795a5e4d937443be785d39e268644d3e5bb9
RonyTCdev <rtcardenas26@gmail.com>
2022-08-30 21:12:51 -0500

    ejercicio 18 js

--- canal-jonmircha/ejercicio2.js
+++ canal-jonmircha/ejercicio2.js
@@ -381,5 +381,39 @@ const calcularYears = (fecha = undefined) => {
 
 
 // 18) Programa una función que dada una cadena de texto cuente el número de vocales y consonantes, pe. miFuncion("Hola Mundo") devuelva Vocales: 4, Consonantes: 5.
+//Curso JavaScript: 40. Ejercicios de Lógica de Programación ( 7 / 10 ) 30/08/22
+
+const contarLetras = (cadena = "") => {
+    if(!cadena) return console.warn("No ingresaste una cadena de texto");
+
+    if(typeof cadena !=="string") return console.error(`El valor "${cadena}" ingresado, NO es una cadena de texto`);
+
+    let vocales = 0,
+    consonantes = 0;
+
+    cadena = cadena.toLocaleLowerCase();
+
+    for (let letra of cadena) {
+        if(/[aeiouáéíóúü]/.test(letra))vocales++;
+
+        if(/[bcdfghjklmnpqrstvwxyz]/.test(letra)) consonantes++;
+    
+    }
+
+    return console.info({
+        cadena,
+        vocales,
+        consonantes
+    })
+
+}
+
+// contarLetras();
+// contarLetras(3);
+// contarLetras("Hola mundo");
+// contarLetras("Noño");
+
+
+
 // 19) Programa una función que valide que un texto sea un nombre válido, pe. miFuncion("Jonathan MirCha") devolverá verdadero.
 // 20) Programa una función que valide que un texto sea un email válido, pe. miFuncion("jonmircha@gmail.com") devolverá verdadero.
