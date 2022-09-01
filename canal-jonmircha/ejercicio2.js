cdd0e790a9c6f8a41c4b17ad5859e81b789260bb
RonyTCdev <rtcardenas26@gmail.com>
2022-08-31 22:59:34 -0500

    js

--- canal-jonmircha/ejercicio2.js
+++ canal-jonmircha/ejercicio2.js
@@ -458,6 +458,7 @@ const validarEmail = (email = "") => {
 
 // 21) Programa una función que dado un array numérico devuelve otro array con los números elevados al cuadrado, pe. mi_funcion([1, 4, 5]) devolverá [1, 16, 25].
 
+//prueba
 
 
 
