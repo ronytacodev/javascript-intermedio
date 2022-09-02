282eabb24c1186cfecbf2d4827bff6ff5eecfafa
RonyTCdev <rtcardenas26@gmail.com>
2022-09-01 22:17:53 -0500

    ej 21

--- canal-jonmircha/ejercicio2.js
+++ canal-jonmircha/ejercicio2.js
@@ -457,8 +457,31 @@ const validarEmail = (email = "") => {
 // validarEmail("hercules.neron@gmail.com");
 
 // 21) Programa una función que dado un array numérico devuelve otro array con los números elevados al cuadrado, pe. mi_funcion([1, 4, 5]) devolverá [1, 16, 25].
+//Curso JavaScript: 41. Ejercicios de Lógica de Programación ( 8 / 10 ) 01/09/22
 
-//prueba
+const devolverCuadrados = (arr = undefined) => {
+    if(arr === undefined) return console.warn("No ingresaste un arreglo de números");
+
+    if(!(arr instanceof Array)) return console.error("El valor que ingresaste no es un arreglo");
+
+    if(arr.length === 0) return console.error("El arreglo esta vacío");
+
+    for(let num of arr) {
+        if(typeof num !== "number") return console.error(`El valor "${num}" ingresado, NO es un número`);
+    }
+
+    const newArr = arr.map(el => el*el);
+
+    return console.info(`Arreglo original: ${arr},\n Arreglo elevado al cuadrado: ${newArr}`);
+}
+
+devolverCuadrados();
+devolverCuadrados(true);
+devolverCuadrados([]);
+devolverCuadrados([1, 3, "4"]);
+devolverCuadrados([1,2,3]);
+
+
 
 
 
