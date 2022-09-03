5851bade3559efa4a03a86a38908e96ca6f483e4
RonyTCdev <rtcardenas26@gmail.com>
2022-09-02 22:52:52 -0500

    avance ej 27 js

--- canal-jonmircha/ejercicio2.js
+++ canal-jonmircha/ejercicio2.js
@@ -625,8 +625,52 @@ const quitarDuplicados = (arr = undefined) => {
     );
  }
 
- promedio();
- promedio({});
- promedio([]);
- promedio([2, true]);
- promedio([1,2,3,4,5,6,7,8,9,0]);
+//  promedio();
+//  promedio({});
+//  promedio([]);
+//  promedio([2, true]);
+//  promedio([1,2,3,4,5,6,7,8,9,0]);
+
+
+// 27) Programa una clase llamada Pelicula.
+
+// La clase recibirá un objeto al momento de instanciarse con los siguentes datos: id de la película en IMDB, titulo, director, año de estreno, país o países de origen, géneros y calificación en IMBD.
+//   - Todos los datos del objeto son obligatorios.
+//   - Valida que el id IMDB tenga 9 caracteres, los primeros 2 sean letras y los 
+//      7 restantes números.
+//   - Valida que el título no rebase los 100 caracteres.
+//   - Valida que el director no rebase los 50 caracteres.
+//   - Valida que el año de estreno sea un número entero de 4 dígitos.
+//   - Valida que el país o paises sea introducidos en forma de arreglo.
+//   - Valida que los géneros sean introducidos en forma de arreglo.
+//   - Valida que los géneros introducidos esten dentro de los géneros 
+//      aceptados*.
+//   - Crea un método estático que devuelva los géneros aceptados*.
+//   - Valida que la calificación sea un número entre 0 y 10 pudiendo ser 
+//     decimal de una posición.
+//   - Crea un método que devuelva toda la ficha técnica de la película.
+//   - Apartir de un arreglo con la información de 3 películas genera 3 
+//     instancias de la clase de forma automatizada e imprime la ficha técnica 
+//     de cada película.
+
+// * Géneros Aceptados: Action, Adult, Adventure, Animation, Biography, Comedy, Crime, Documentary ,Drama, Family, Fantasy, Film Noir, Game-Show, History, Horror, Musical, Music, Mystery, News, Reality-TV, Romance, Sci-Fi, Short, Sport, Talk-Show, Thriller, War, Western.
+
+//Curso JavaScript: 43. Ejercicios de Lógica de Programación ( 10 / 10 ) 02/09/22 hasta el min 12
+
+class Pelicula {
+    constructor({id,titulo,director,estreno,pais,generos,calificacion}) {
+        this.id = id;
+        this.titulo = titulo;
+        this.director = director;
+        this.estreno = estreno;
+        this.pais = pais;
+        this.generos = generos;
+        this.calificacion = calificacion;
+    }
+
+
+}
+
+const peli = new Pelicula({
+    
+})
