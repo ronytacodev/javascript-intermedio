c5e07715aa654a5af132bfa81e0952b5a1454611
RonyTCdev <rtcardenas26@gmail.com>
2022-09-02 21:42:22 -0500

    ej 24 js

--- canal-jonmircha/ejercicio2.js
+++ canal-jonmircha/ejercicio2.js
@@ -530,9 +530,46 @@ const separarParesImpares = (arr = undefined) => {
 
 }
 
-separarParesImpares();
-separarParesImpares("hola");
-separarParesImpares([]);
-separarParesImpares([2,3,true]);
-separarParesImpares([1,2,3,4,5,6,7,8,9,0]);
+// separarParesImpares();
+// separarParesImpares("hola");
+// separarParesImpares([]);
+// separarParesImpares([2,3,true]);
+// separarParesImpares([1,2,3,4,5,6,7,8,9,0]);
 
+
+
+// 24) Programa una función que dado un arreglo de números devuelva un objeto con dos arreglos, el primero tendrá los numeros ordenados en forma ascendente y el segundo de forma descendiente, pe. miFuncion([7, 5,7,8,6]) devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5] }.
+// Curso JavaScript: 42. Ejercicios de Lógica de Programación ( 9 / 10 ) 02/09/22 hasta el min 9
+
+const ordenarArreglo = (arr = undefined) => {
+    if(arr === undefined) return console.warn("No ingresaste un arreglo de números");
+
+    if(!(arr instanceof Array)) return console.error("El valor que ingresaste no es un arreglo");
+
+    if(arr.length === 0) return console.error("El arreglo esta vacío");
+
+    for(let num of arr) {
+        if(typeof num !== "number") return console.error(`El valor "${num}" ingresado, NO es un número`);
+    }
+
+    return console.info({
+        arr,
+        asc: arr.map(el => el).sort(),
+        desc: arr.map(el => el).sort().reverse()
+    });
+
+}
+
+ordenarArreglo();
+ordenarArreglo("hola");
+ordenarArreglo([]);
+ordenarArreglo([3,{}]);
+ordenarArreglo([7,5,7,8,6]);
+
+
+
+
+
+// 25) Programa una función que dado un arreglo de elementos, elimine los duplicados, pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true]) devolverá ["x", 10, 2, "10", true].
+// 26) Programa una función que dado un arreglo de números obtenga el promedio, pe. promedio([9,8,7,6,5,4,3,2,1,0]) devolverá 4.5.
+ 
