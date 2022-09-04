0b63682aaec7b2486cab88ad01bb83c69b59e367
RonyTCdev <rtcardenas26@gmail.com>
2022-09-03 19:26:35 -0500

    avance ej 27

--- canal-jonmircha/ejercicio2.js
+++ canal-jonmircha/ejercicio2.js
@@ -666,11 +666,27 @@ class Pelicula {
         this.pais = pais;
         this.generos = generos;
         this.calificacion = calificacion;
+
+        this.validarIMDB(id);
+
     }
 
+    validarCadena(propiedad, valor){
+        if (!valor) return console.warn(`${propiedad} "${valor}" está vacío`);
 
+        if (typeof valor !== "string") return console.error(`${propiedad} "${valor}" ingresado, NO es una cadena de texto`);
+
+        return true;
+    }
+
+    validarIMDB(id){
+        if(this.validarCadena("IMDB id", id))
+            if(!(/^([a-z]){2}([0-9]){7}$/.test(id)))
+            return console.error(`IMDB id "${id}" no es válido, debe tener 9 caracteres, los 2 primeras letrasminúsculas, los 7 restantes números.`);   
+    }
 }
 
+
 const peli = new Pelicula({
-    
-})
+    id:"tt1234567"
+});
