95c4e31040aded3d6035e8bee066d0e46574f0d7
RonyTCdev <rtcardenas26@gmail.com>
2022-09-04 17:27:31 -0500

    promises capitulo 47

--- canal-jonmircha/ejercicio2.js
+++ canal-jonmircha/ejercicio2.js
@@ -840,3 +840,48 @@ const misPelis = [
 // Curso JavaScript: 45. Asincronía y el Event Loop 04/09/22 teoria 
 
 // Curso JavaScript: 46. Callbacks 04/09/22 teoria
+
+// Curso JavaScript: 47. Promesas 04/09/22 
+
+function cuadradoPromise(value) {
+    if(typeof value !=="number"){
+        return Promise.reject(`Error, el valor "${value}" ingresado no es un número`);
+    }
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+           resolve({
+            value,
+            result:value*value
+           }); 
+        }, 0 | Math.random() * 1000);
+    });
+}
+
+cuadradoPromise(0)
+.then(obj=>{
+    console.log("Inicio Promise");
+    console.log(`Promise: ${obj.value}, ${obj.result}`);
+    return cuadradoPromise(1);
+})
+.then(obj=>{
+    console.log(`Promise: ${obj.value}, ${obj.result}`);
+    return cuadradoPromise(2);
+})
+.then(obj=>{
+    console.log(`Promise: ${obj.value}, ${obj.result}`);
+    return cuadradoPromise("3"); 
+})
+.then(obj=>{
+    console.log(`Promise: ${obj.value}, ${obj.result}`);
+    return cuadradoPromise(4); 
+})
+.then(obj=>{
+    console.log(`Promise: ${obj.value}, ${obj.result}`);
+    return cuadradoPromise(5); 
+})   
+.then(obj=>{
+    console.log(`Promise: ${obj.value}, ${obj.result}`); 
+    console.log("Fin Promise");    
+})
+.catch(err=>console.error(err));        
